dadccb9531bbb8edbc4267cf09f036fc38b59d95
Leandro Henrique <leandro.h22@hotmail.com>
2025-03-19 14:27:37 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //short numeroCurto2 = numeroNormal; // não vai deixar
+        short numeroCurto2 = (short) numeroNormal; // forçando "casting"
+       //adicionar um novo objeto a classe Date dataNacimento = new Date();
+        char sexo = 'M';
+        final double VALOR_DE_PI = 3.14; // final e caixa alta está indicando que não vai mudar o valor.
     }
 }
